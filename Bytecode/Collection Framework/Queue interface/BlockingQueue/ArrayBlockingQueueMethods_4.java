96435df86b0735203515f074d4cc9d35474b3487
G1Hu6 <r1ruru219@gmail.com>
2023-09-09 17:40:23 +0530

    origin master

--- Bytecode/Collection Framework/Queue interface/BlockingQueue/ArrayBlockingQueueMethods_4.java
+++ Bytecode/Collection Framework/Queue interface/BlockingQueue/ArrayBlockingQueueMethods_4.java
@@ -1,64 +1,62 @@
 import java.util.concurrent.*;
 import java.util.*;
 
-class BlockingQueueMethods{
+class ArrayBlockingQueueMethods_4{
         public static void main(String[] arsg)throws InterruptedException{
-                //BlockingQueue bQueue = new ArrayBlockingQueue();
-                //error: no suitable constructor found for ArrayBlockingQueue(no arguments)
+
+		// ArrayBlockingQueue Constructors :
+
+		// 1.ArrayBlockingQueue(int capacity) => Creates an ArrayBlockingQueue with the given (fixed) capacity and default access policy(false).
 
                 BlockingQueue bQueue = new ArrayBlockingQueue(4);
-                
-	// 1.public abstract boolean add(E);
-		
-		//bQueue.add(null);	//Exception in thread "main" java.lang.NullPointerException
-
-		//bQueue.add(10);
-		//bQueue.add("String");
-		//System.out.println(bQueue.add(50));	// true
-							//returning true upon success and throwing an IllegalStateException if no space is currently available
-	
-
-	// 2.public abstract boolean offer(E);
+                bQueue.offer(10);
                 bQueue.offer(20);
                 bQueue.offer(30);
                 bQueue.offer(40);
-		bQueue.offer(40);
-		//bQueue.offer(40);
 
                 System.out.println(bQueue);
-		System.out.println(bQueue.remainingCapacity());
 
-		//bQueue.add(50);		//Exception in thread "main" java.lang.IllegalStateException: Queue full
-
-
-	// 3.public abstract boolean offer(E, long, java.util.concurrent.TimeUnit) throws java.lang.InterruptedException;
-                bQueue.offer(50 ,5 ,TimeUnit.SECONDS);	//main thread sleeps for 5 sec. 
-            	//Inserts the specified element into this queue, waiting up to the specified wait time if necessary for space to become available.		
-		System.out.println(bQueue);
-
+		// 2.ArrayBlockingQueue(int capacity, boolean fair) =>Creates an ArrayBlockingQueue with the given (fixed) capacity and the specified access policy.
 		ArrayList al = new ArrayList();
-		System.out.println("ArrayList ="+al);
+		al.add("One");
+		al.add("Two");
 
-	// 4.public abstract int drainTo(java.util.Collection<? super E>);
-		bQueue.drainTo(al);
+		BlockingQueue bQueue2 = new ArrayBlockingQueue(5, true, al);
+		BlockingQueue bQueue3 = new ArrayBlockingQueue(5);
 
-		System.out.println("ArrayList ="+al);
-		System.out.println(bQueue);
+		bQueue2.offer(20);
+		bQueue2.offer(20);
+		bQueue2.offer(20);
+		System.out.println(bQueue2);
 
-	// 5. public abstract E take() throws java.lang.InterruptedException;
-		BlockingQueue bQueue2 = new ArrayBlockingQueue(4);
-		bQueue2.remove(10);	//If Queue is empty then nither throw error nor wait.
+		// 3.public E poll();
+		bQueue2.poll();
+		System.out.println(bQueue2);
+
+		// 4.public E poll(long, java.util.concurrent.TimeUnit) throws java.lang.InterruptedException;
+		bQueue3.poll(5, TimeUnit.SECONDS);
+                System.out.println(bQueue3);
+
+		// 5.public E peek();
+		System.out.println(bQueue2.peek());
+		System.out.println(bQueue2);
+
+		// 6.public int size();
+		System.out.println(bQueue2.size());
+
+		// 7.public boolean contains(java.lang.Object);
+		System.out.println(bQueue2.contains(20));
+
+		// 8.public java.lang.Object[] toArray();
+		Object obj[] = bQueue2.toArray();
+		for(var data : obj){
+			System.out.println(data);
+		}
+
+		// 9.public void clear();
+		bQueue2.clear();
+		System.out.println(bQueue2);
 		
-	// 6.public abstract E take() throws java.lang.InterruptedException;
-		//bQueue2.take();	//wait untill the element is added in Queue.
-	
-	// 7.public abstract int remainingCapacity();
-		System.out.println(bQueue.remainingCapacity());	        //4 (Because of using the drainTo() method bQueue becomes empty)
-		System.out.println(bQueue2.remainingCapacity());	//4
 
-	// 8.public abstract E poll(long, java.util.concurrent.TimeUnit) throws java.lang.InterruptedException;
-		//Retrieves and removes the head of this queue, waiting up to the specified wait time if necessary for an element to become available.
-
-
-        }
-}
+	}
+}	
